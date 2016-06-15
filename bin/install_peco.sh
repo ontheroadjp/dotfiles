3cc09b9da27024e6fc52355b8e37f2211b1bacde
@ontheroad_jp <dev@ontheroad.jp>
2016-06-15 15:53:07 +0900

    [Clean]

--- bin/install_peco.sh
+++ bin/install_peco.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-wget https://github.com/peco/peco/releases/download/v0.2.9/peco_linux_amd64.tar.gz
-tar xvzf peco_linux_amd64.tar.gz
-sudo cp peco_linux_amd64/peco /usr/local/bin
-sudo chmod 111 /usr/local/bin/peco
+# ------------------------------------------
+# Function
+# ------------------------------------------
+function _install_peco() {
+    if hash "wget" && hash "tar"; then
+        echo -n "install peco ..."
+        wget https://github.com/peco/peco/releases/download/v0.2.9/peco_linux_amd64.tar.gz
+        tar xvzf peco_linux_amd64.tar.gz
+        sudo cp peco_linux_amd64/peco /usr/local/bin
+        sudo chmod 111 /usr/local/bin/peco
+        rm -rf peco_linux_amd64*
+        echo "done."
+    else
+        echo "wget and/or tar doesn't installed."
+        exit 1
+    fi
+}
+
+# ------------------------------------------
+# Main
+# ------------------------------------------
+_install_peco
+exit 0
