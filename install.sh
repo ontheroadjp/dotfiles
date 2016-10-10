c95a9f52f0168c3fb4a4381c5f18cf8ad36a3d94
@ontheroad_jp <dev@ontheroad.jp>
2016-10-10 18:28:03 +0900

    [Fix] function _install_peco()

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ DOTPATH=~/dotfiles
 ## -----------------------------------
 ## dotfiles の取得
 ## -----------------------------------
-#
 ## git が使えるなら git
 #if hash "git"; then
 #    git clone --recursive "$GITHUB_URL" "$DOTPATH"
@@ -35,123 +34,123 @@ DOTPATH=~/dotfiles
 #    die "not found: $DOTPATH"
 #fi
 
-## -----------------------------------
-# デプロイ（Mac）
-## -----------------------------------
-if [ "$(uname)" == 'Darwin' ]; then 
-	echo Welcome to MacOSX !
-
-	for f in .bash_profile .bashrc .gitconfig .gitignore_global .tmux.conf .vim .vimrc 
-	do
-		[ "$f" = ".git" ] && continue
-		if [ -e $DOTPATH/$F ]; then
-			ln -snfv "$DOTPATH/$f" "$HOME/$f" > /dev/null
-			echo "success: ${f}"
-		else
-			echo "missing file/directory: ${f}"
-		fi
-	done
-
-	## gvim(MacVim)
-	#ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-	#ln -sf /usr/share/vim/vim73/colors/desert.vim ~/.vim/colors/
-
-	# Karabiner
-	ln -sf ~/dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml > /dev/null
-	echo "success: Karabiner - private.xml"
-
-	# peco
-    peco_install_dir="/usr/local/bin"
-    #if [ ! -e $peco_install_dir ]; then
-    if hash "peco"; then
-        wget https://github.com/peco/peco/releases/download/v0.2.9/peco_linux_amd64.tar.gz
-        tar xvzf peco_linux_amd64.tar.gz
-        sudo cp peco_linux_amd64/peco /usr/local/bin
-        sudo chmod 111 /usr/local/bin/peco
-	    ln -sf ~/dotfiles/peco ~/.config/peco
-        rm peco_linux_amd64.tar.gz
-        rm -rf peco_linux_amd64
-	    echo "success: Peco installed!"
+function _deploy_dotfiles() {
+    #-------------------------------------------------
+    # for MacOSX
+    #-------------------------------------------------
+    if [ "$(uname)" == "Darwin" ]; then 
+    	echo "Welcome to MacOSX !"
+    
+        echo ">>> deploy dotfiles..."
+        dotfiles=(.bash_profile .bashrc .gitconfig .gitignore_global .tmux.conf .vim .vimrc)
+    	for f in ${dotfiles[@]}
+    	do
+    		[ "${f}" = ".git" ] && continue
+    		if [ -e ${DOTPATH}/${F} ]; then
+    			ln -snfv "${DOTPATH}/${f}" "${HOME}/${f}" > /dev/null
+    			echo "success: ${f}"
+    		else
+    			echo "missing file/directory: ${f}"
+    		fi
+    	done
+    
+    	## gvim(MacVim)
+    	#ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+    	#ln -sf /usr/share/vim/vim73/colors/desert.vim ~/.vim/colors/
+    
+    	# Karabiner
+    	ln -sf ~/dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml > /dev/null
+    	echo "success: Karabiner - private.xml"
+    
+    #-------------------------------------------------
+    # for Linux
+    #-------------------------------------------------
+    elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    	echo "Welcome to Linux !"
+    
+        echo ">>> deploy dotfiles..."
+        dotfiles=(.bash_profile .bashrc .gitconfig .gitignore_global .vim .vimrc .gvimrc)
+    	for f in ${dotfiles[@]}
+    	do
+    		[ "${f}" = ".git" ] && continue
+    		if [ -e ${DOTPATH}/${F} ]; then
+    			ln -snfv "${DOTPATH}/${f}" "${HOME}/${f}" > /dev/null
+    			echo "success: ${f}"
+    		else
+    			echo "missing file/directory: ${f}"
+    		fi
+    	done
+    
+    #-------------------------------------------------
+    # for Windows(Cygwin)
+    #-------------------------------------------------
+    elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+    	echo 'Welcome to Cygwin!'
     else
-        echo 'skip Peco Install'
-    fi
-
-#-------------------------------------------------
-# デプロイ（Linux）
-#-------------------------------------------------
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-	echo 'Welcome to Linux !'
-
-	for f in .bash_profile .bashrc .gitconfig .gitignore_global .vim .vimrc .gvimrc
-	do
-		[ "$f" = ".git" ] && continue
-		if [ -e $DOTPATH/$F ]; then
-			ln -snfv "$DOTPATH/$f" "$HOME/$f" > /dev/null
-			echo "success: ${f}"
-		else
-			echo "missing file/directory: ${f}"
-		fi
-	done
-
-#-------------------------------------------------
-# デプロイ（Windows(Cygwin) ）
-#-------------------------------------------------
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
-	echo 'Welcome to Cygwin!'
-else
-#-------------------------------------------------
-# デプロイ（その他 OS）
-#-------------------------------------------------
-	echo "Your platform ($(uname -a)) is not supported."
-	exit 1
-fi
-
-#-------------------------------------------------
-# 共通
-#-------------------------------------------------
-# Install peco
-function _install_peco() {
-    if ! hash "peco"; then
-        echo -n '>>> peco Install...'
-        wget https://github.com/peco/peco/releases/download/v0.2.9/peco_linux_amd64.tar.gz
-        tar xvzf peco_linux_amd64.tar.gz
-        sudo cp peco_linux_amd64/peco /usr/local/bin
-        sudo chmod 111 /usr/local/bin/peco
-        rm -rf peco_linux_amd64*
-        echo "done."
-    else
-        echo '>>> skip peco install'
+    #-------------------------------------------------
+    # for other OS
+    #-------------------------------------------------
+    	echo "Your platform ($(uname -a)) is not supported."
+    	exit 1
     fi
 }
 
-# Install NeoBundle for vim 
+#-------------------------------------------------
+# Vim
+#-------------------------------------------------
 function _install_neobundle() {
+    printf ">>> install NeoBundle for vim..."
     if hash "git" && test ! -e ~/dotfiles/.vim/bundle/neobundle.vim; then
-        echo -n '>>> NeoBundle Install...'
         git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
         vim + ":NeoBundleInstall" +:q
         echo "done."
     else
-        echo '>>> skip NeoBundleInstall'
+        echo "skip"
     fi
 }
 
-# Install dopecker(https://github.com/ontheroadjp/dopecker.git)
-function _install_dopecker() {
-    if hash git && [ ! -d ~/dotfiles/dopecker ]; then
-        echo -n '>>> dopecker Install...'
-        git clone https://github.com/ontheroadjp/dopecker.git ~/dotfiles/dopecker
-        echo "done."
+#-------------------------------------------------
+# Peco
+#-------------------------------------------------
+function _install_peco() {
+    printf ">>> install peco..."
+    if ! hash "peco"; then
+        if [ "$(uname)" == 'Darwin' ]; then 
+            wget https://github.com/peco/peco/releases/download/v0.4.3/peco_darwin_386.zip -P ${DOTPATH} > /dev/null 2>&1
+            tar xzf peco_darwin_386.zip -C peco --strip-components 1
+            rm ${DOTPATH}/peco_darwin_386.zip
+        elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+            wget https://github.com/peco/peco/releases/download/v0.2.9/peco_linux_amd64.tar.gz -P ~/dotfiles > /dev/null 2>&1
+            tar xzf peco_linux_amd64.tar.gz -C peco --strip-components 1
+            rm ${DOTPATH}/peco_linux_amd64.tar.gz
+        fi
+        mkdir -p ${HOME}/.peco && ln -sf ~/dotfiles/peco/config.json ~/.peco/config.json
+	    echo "done"
     else
-        echo '>>> skip dopecker ienstall'
+        echo "skip"
     fi
 }
 
+#-------------------------------------------------
+# Docker
+#-------------------------------------------------
+function _install_docker_dd() {
+    printf ">>> install docker-dd..."
+    if hash git && [ ! -d ~/dotfiles/docker-dd ]; then
+        echo -n '>>> docker-dd Install...'
+        git clone https://github.com/nutsllc/docker-dd.git
+        #git clone https://github.com/ontheroadjp/dopecker.git ~/dotfiles/dopecker
+        echo "done"
+    else
+        echo 'skip'
+    fi
+}
 
+_deploy_dotfiles
+#_install_neobundle
 _install_peco
-_install_dopecker
-echo '>>> load .bash_profile'
-source ~/.bash_profile >> /dev/null >&2>1
-echo 'Complete!'
+_install_docker_dd
+
+echo "Complete! - note:you must source ~/.bash_profile"
 
 exit 0
